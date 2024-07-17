e84fc8bf29d9d1a79e0fce00b5e73b7e6811faa6
nirvana47 <nirvana47@gmail.com>
2024-07-16 22:22:15 -0700

    Delete test_download.py

--- test_download.py
+++
@@ -1,6 +0,0 @@
-from download import download_video
-
-url = "https://www.youtube.com/watch?v=9FTajHP4JoQ"
-
-output_path = download_video(url)
-print(f"Downloaded and converted file: {output_path}")
